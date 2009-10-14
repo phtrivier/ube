1c6557fc8de2eb291c46ccf774ca4a70f1ddc9d0
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2009-10-14 23:35:17 +0200

    Added minimal SDL support.

--- src/client/ube.cpp
+++ src/client/ube.cpp
@@ -9,6 +9,10 @@
 #include <cstdio>
 using namespace std;
 
+#include "SDL/SDL.h"   /* All SDL apps need this */
+#include <stdio.h>
+#include <stdlib.h> /* for exit() */
+
 int main(int argc, char ** argv) {
   
   // printf("This is %s. Thanks for watching\n", PACKAGE_STRING);
@@ -25,4 +29,24 @@ int main(int argc, char ** argv) {
   Greeter g("PHT");
   g.greet();
   
+  SDL_Surface* screen = NULL;
+
+  /* Initialize defaults, Video and Audio subsystems */
+  if((SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)==-1)) { 
+    printf("Could not initialize SDL: %s.\n", SDL_GetError());
+    exit(-1);
+  }
+  
+  /* Clean up on exit */
+  atexit(SDL_Quit);
+
+  screen = SDL_SetVideoMode(640, 480, 8, SDL_SWSURFACE);
+  if ( screen == NULL ) {
+    fprintf(stderr, "Couldn't set 640x480x8 video mode: %s\n", SDL_GetError());
+    exit(1);
+  }
+  
+  sleep(5);
+
+  exit(0);
 }
