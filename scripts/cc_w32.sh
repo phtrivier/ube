0ba5f5cb552b1f7f34f8a2128864400fccff0ff2
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2010-02-16 17:40:01 +0100

    Cross compiling script should now works ...

--- scripts/cc_w32.sh
+++ scripts/cc_w32.sh
@@ -5,7 +5,7 @@
 
 # Change this path as appropriate
 #export MW32=$HOME/sandbox/src/mingw-cross-env
-export MW32=$HOME/src/mingw-cross-env-2.10
+export MW32=$HOME/sandbox/mingw-cross-env-trunk
 
 # Tools from mingw-cross-env should be available
 export PATH=$MW32/usr/bin:$MW32/usr/i686-pc-mingw32/bin:$PATH
@@ -57,7 +57,7 @@ prepare_folders() {
 build_cc() {
     autoreconf
     cd $BUILD_FOLDER
-    ../../../configure  --includedir=$MW32/usr/i686-pc-mingw32/include --oldincludedir=$MW32/usr/i686-pc-mingw32/include --prefix $INSTALL_FOLDER --host=i686-pc-mingw32 --build=i686-pc-linux-gnu 
+    ../../../configure  --with-boost=$MW32/usr/i686-pc-mingw32/include --with-boost-lib-dir=$MW32/usr/i686-pc-mingw32/lib --prefix $INSTALL_FOLDER --host=i686-pc-mingw32 --build=i686-pc-linux-gnu 
     make  && make install && rm -rf $BUILD_FOLDER && rm -rf $LATEST && ln -sf $INSTALL_FOLDER $LATEST
 }
 
