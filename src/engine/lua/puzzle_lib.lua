86ed97f74777b4910d51bf42ae946cdd9a12a653
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2010-03-18 23:10:27 +0100

    Extracted things in libs

--- src/engine/lua/puzzle_lib.lua
+++ src/engine/lua/puzzle_lib.lua
@@ -2,6 +2,8 @@
 package.path = ube_engine_lua_path or package.path
 
 require 'move'
+require 'script'
+require 'pick_move'
 
 -- Set the dimension of globaly available puzzle.
 function set_puzzle_dimensions(w,h)
@@ -26,25 +28,4 @@ end
 
 function add_puzzle_script(i,j,index)
    cpp_puzzle_add_script(cpp_puzzle, cpp_puzzle_loader, i,j,index)
-end
-
-g_scripts = {};
-
-function register_script(index, script)
-   print("Registering script at index " .. index)
-   g_scripts[index] = script;
-end
-
-function do_script(index, puzzle)
-   print("Doing script at " .. index)
-   if (g_scripts[index] ~= nil) then
-      g_scripts[index]:execute(puzzle)
-   end
-end
-
-function undo_script(index, puzzle)
-   print("Undoing script at " .. index)
-   if (g_scripts[index] ~= nil) then
-      g_scripts[index]:undo(puzzle)
-   end
 end
--- src/engine/tests/lua/puzzle_loader_test/puzzle_with_scripts.lua
+++ src/engine/tests/lua/puzzle_loader_test/puzzle_with_scripts.lua
@@ -8,27 +8,6 @@ set_puzzle_rows({"###",
 
 set_puzzle_moves({Move.SINGLE})
 
-PickMoveScript = {}
-
-function PickMoveScript:new(type) 
-   o = {
-      move_type = type,
-      move_index = nil
-   }
-   setmetatable(o, self)
-   self.__index = self
-   return o
-end
-
-function PickMoveScript:execute(puzzle)
-   print("executing pickmove cmd")
-   self.move_index = cpp_puzzle_add_move(puzzle, self.move_type)
-end
-
-function PickMoveScript:undo(puzzle)
-   print("undoing pickmove cmd")
-   cpp_puzzle_remove_move(puzzle, self.move_index)
-end
 
 -- FIXME(pht) : of course, re-arrange this
 register_script(1, PickMoveScript:new(Move.DOUBLE))
