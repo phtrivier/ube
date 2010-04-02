ea4d3b4f3b377491e550b2d8c05259a3c8fc3ab4
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2010-04-02 22:23:52 +0200

    simplified scripts

--- scripts/git-merge-feature.sh
+++ scripts/git-merge-feature.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 # Helper script to merge feature back into the dev branch
 # Uses --no-ff to keep all the commits inside the history
-delete() {
-    echo "Deleting branch..."
-    git branch -D $1
-}
-
-push() {
-    echo "Pushing branch..."
-    git push origin dev
-}
-
-skip() {
-    echo
-}
-
 if [ ! $1 ]
 then
     echo "Please give an argument"
@@ -24,36 +10,11 @@ fi
 echo "Merging..."
 git checkout dev
 git merge --no-ff $1
+echo "Deleting branch..."
+git branch -D $1
+echo "Pushing..."
+git push origin dev
 
-echo "Do you want to delete the branch ? (y/n) [N]"
-read REPLY
-echo
-case $REPLY in
-    y | Y)
-	delete
-	;;
-    n | N)
-	skip
-	;;
-    *)
-	skip
-	;;
-esac
-
-echo "Do you want to push the branch ? (y/n) [N]"
-read REPLY
-echo
-case $REPLY in
-    y | Y)
-	push
-	;;
-    n | N)
-	skip
-	;;
-    *)
-	skip
-	;;
-esac
 
 
 
