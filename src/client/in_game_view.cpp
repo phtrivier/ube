3767f51afbb13a3f537559cae39cdd28e0cad8f6
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2010-04-10 17:43:15 +0200

    Different images for the paths themselves (will make it easier to see overlays)

--- src/client/in_game_view.cpp
+++ src/client/in_game_view.cpp
@@ -111,7 +111,8 @@ InGameView::render_selected_cell(const Puzzle & i_puzzle) {
     if (i != -1) {
       if (i_puzzle.is_valid_position(i,j)) {
 	assert(i_puzzle.get_cell_at(i,j) != NULL);
-	if (!i_puzzle.get_cell_at(i,j)->is_empty()) {
+	if (!i_puzzle.get_cell_at(i,j)->is_empty() &&
+	    !i_puzzle.get_cell_at(i,j)->is_in_path()) {
 	  dep_renderer_.render_banned_cell(i,j);
 	}
       }
