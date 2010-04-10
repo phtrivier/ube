84bf2496deea0f17c8cfb0ac8886a41cc7036369
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2010-04-10 15:55:18 +0200

    Fixed image conversion script

--- scripts/convert_images.rb
+++ scripts/convert_images.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/ruby
 
-if (ARGV.size < 0)
+if (ARGV.size < 1)
   puts "Usage : ./scripts/convert_images.rb data/images move"
 end
 
 ["path", "move", "overlay"].each do |image_type|
   
-  Dir.glob("#{ARGV[0]}/#{image_type}_*.xcf").each do |filename|
+  Dir.glob("#{ARGV[0]}/xcf/#{image_type}_*.xcf").each do |filename|
     basename = File.basename(filename, '.xcf')
-    newname = File.dirname(filename) + "/" + basename + ".png"
+    newname = File.dirname(filename) + "/png/" + basename + ".png"
     cmd = "convert -flatten #{filename} #{newname}" 
     puts(cmd)
     system(cmd)
